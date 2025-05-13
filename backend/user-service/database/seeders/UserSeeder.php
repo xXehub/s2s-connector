9e3b45c3b9d90717a92eafd6206b75f43cfe3b20
xXehub <artixsanni@gmail.com>
2025-05-13 16:49:51 +0700

    Update: Menambahkan user factory dan middleware pada api userController

--- backend/user-service/database/seeders/UserSeeder.php
+++ backend/user-service/database/seeders/UserSeeder.php
@@ -9,12 +9,23 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::insert([
+        $defaultUsers = [
             ['name' => 'Dailam', 'email' => 'dailam@example.com'],
             ['name' => 'Angela', 'email' => 'angela@example.com'],
             ['name' => 'Sihub', 'email' => 'sihub@example.com'],
-        ]);
+        ];
 
-        User::factory()->count(10)->create();
+        foreach ($defaultUsers as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']], 
+                ['name' => $user['name']]    
+            );
+        }
+
+        // Tambahkan 10 user 
+        User::factory()
+            ->count(10)
+            ->sequence(fn ($sequence) => ['email' => "user{$sequence->index}@example.com"])
+            ->create();
     }
 }
--- backend/user-service/routes/api.php
+++ backend/user-service/routes/api.php
@@ -15,4 +15,6 @@ use App\Http\Controllers\UserController;
 */
 
 
-Route::apiResource('users', UserController::class);
+Route::middleware('api')->group(function () {
+    Route::apiResource('users', UserController::class);
+});
